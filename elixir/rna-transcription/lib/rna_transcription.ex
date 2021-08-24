dccd83aa35ec47648f7c789b36fd95b6ab6102b1
Ryan Tracey <ryan.tracey@gmail.com>
2021-08-24 11:58:11 +0200

    Initial commit

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,20 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+
+  @spec to_rna([char]) :: [char]
+  def to_rna() do '' end
+  def to_rna(dna) do
+    dna_rna_map = %{
+      ?G => ?C,
+      ?C => ?G,
+      ?T => ?A,
+      ?A => ?U
+    }
+    dna |> Enum.map(&(dna_rna_map[&1]))
+  end
+end
